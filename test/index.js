c449233e406562ec1eaf75be900ecae022f91770
Eric Redon <eric.redon@coox.org>
2016-03-14 19:54:55 +0100

    feat(meta): introduce `npm test` for pre-build unit testing

---
+++ test/index.js
@@ -0,0 +1,16 @@
+import chai from 'chai';
+import region from '../src/region';
+
+const expect = chai.expect;
+
+describe('SurveyGizmo REST API — region', () => {
+
+  it('should define EU', () => {
+    expect(region.EU).to.not.be.undefined;
+  });
+
+  it('should define US', () => {
+    expect(region.US).to.not.be.undefined;
+  });
+
+});
