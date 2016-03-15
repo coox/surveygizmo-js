f18320096d4878904dbbb0ce12e67a3c35e850c9
Eric Redon <eric.redon@coox.org>
2016-03-15 16:54:54 +0100

    feat(region): export ìsValid` validator function

--- src/lib/region.js
+++ src/lib/region.js
@@ -5,7 +5,18 @@
  * https://apihelp.surveygizmo.com/help/article/link/us-or-eu-api
  */
 
+const EU = 'EU';
+const US = 'US';
+
+const _validRegions = {
+  EU,
+  US,
+};
+
+const isValid = (region) => typeof(_validRegions[region]) !== 'undefined';
+
 export default {
-  EU: 'EU',
-  US: 'US',
+  EU,
+  US,
+  isValid,
 };
--- src/test/index.js
+++ src/test/index.js
@@ -1,10 +1,19 @@
 import chai from 'chai';
+import libClient from '../lib/client';
 import libDomain from '../lib/domain';
 import libRegion from '../lib/region';
 
 const expect = chai.expect;
 
 describe('SurveyGizmo REST API', () => {
+  describe('lib/client', () => {
+    it('init() should return a client', () => {
+      const client = libClient.init();
+      expect(client).to.not.be.undefined;
+      expect(client.domain).to.not.be.undefined;
+    });
+  });
+
   describe('lib/domain', () => {
     it('get() should look like a surveygizmo domain', () => {
       const domain = libDomain.get();
@@ -15,16 +24,22 @@ describe('SurveyGizmo REST API', () => {
   });
 
   describe('lib/region', () => {
-    it('should define EU', () => {
+    it('should export standard regions', () => {
       expect(libRegion.EU).to.not.be.undefined;
-    });
-
-    it('should define US', () => {
       expect(libRegion.US).to.not.be.undefined;
     });
 
-    it('should not define GARBAGE', () => {
+    it('should not export garbage', () => {
       expect(libRegion.GARBAGE).to.be.undefined;
+    });
+
+    it('should validate standard regions', () => {
+      expect(libRegion.isValid(libRegion.EU)).to.be.true;
+      expect(libRegion.isValid(libRegion.US)).to.be.true;
+    });
+
+    it('should not validate garbage', () => {
+      expect(libRegion.isValid('GARBAGE')).to.be.false;
     });
   });
 });
