4c617ccda25428fc73d7680caac911b22ae903fe
Eric Redon <eric.redon@coox.org>
2016-03-15 11:56:33 +0100

    refactor(meta): clarify test hierarchy

--- src/test/index.js
+++ src/test/index.js
@@ -3,16 +3,18 @@ import region from '../lib/region';
 
 const expect = chai.expect;
 
-describe('SurveyGizmo REST API — region', () => {
-  it('should define EU', () => {
-    expect(region.EU).to.not.be.undefined;
-  });
+describe('SurveyGizmo REST API', () => {
+  describe('region', () => {
+    it('should define EU', () => {
+      expect(region.EU).to.not.be.undefined;
+    });
 
-  it('should define US', () => {
-    expect(region.US).to.not.be.undefined;
-  });
+    it('should define US', () => {
+      expect(region.US).to.not.be.undefined;
+    });
 
-  it('should not define GARBAGE', () => {
-    expect(region.GARBAGE).to.be.undefined;
+    it('should not define GARBAGE', () => {
+      expect(region.GARBAGE).to.be.undefined;
+    });
   });
 });
