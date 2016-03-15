ed6491e51667a1cb8e94841842ee67699cc9ea0d
Eric Redon <eric.redon@coox.org>
2016-03-15 11:40:35 +0100

    feat(meta): introduce environment config
    Encourage the use of environment variables:
    - `SURVEYGIZMO_API_KEY`
    - `SURVEYGIZMO_API_SECRET_KEY`
    
    Twelve-Factor App Config ♥FTW♥

---
+++ src/config/default.js
@@ -0,0 +1,8 @@
+const defaultEnvironmentConfig = {
+  auth: {
+    apiKey: process.env.SURVEYGIZMO_API_KEY,
+    apiSecretKey: process.env.SURVEYGIZMO_API_SECRET_KEY,
+  },
+};
+
+export default defaultEnvironmentConfig;
